5178a56453aa4a0db237366e84b8638c8beea03c
wilypradana <pradanawily28@gmail.com>
2023-09-11 13:25:11 +0700

    add reactrouter on development

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import Coaching from "./component/service/1on1coaching";
-import Homepage from "./component/Homepage";
+import "./index.css";
+import Navbar from "./component/Navbar";
+import Service from "./component/Service";
+import Header from "./component/Header";
+import Learning from "./component/Learning";
+import Footer from "./component/Footer";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Route path="/">element={<Homepage />}</Route>
-      <Route path="/service/1on1-coaching">element={<Coaching />}</Route>
-    </BrowserRouter>
+    <Navbar />
+    <Header />
+    <Service />
+    <Learning />
+    <Footer />
   </React.StrictMode>
 );
 
